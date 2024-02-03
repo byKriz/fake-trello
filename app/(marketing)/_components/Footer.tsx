805ddf840b164ab08ff4acb6ecd7e638d75e6280
byKriz <cristianesquivelh@hotmail.com>
2024-02-02 20:38:42 -0400

    cambiando el pading

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from "@/components/Logo";
 
 export function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full px-4 border-t bg-slate-100">
+    <footer className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className=" space-x-4 md:block md:w-auto flex items-center justify-between w-full">
